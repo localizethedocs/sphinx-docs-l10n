2d4ac0b062770a19030c5823f0a0b5a11501da0c
ltdrobot <ltdrobot@localizethedocs.org>
2026-09-15 14:11:41 +0000

    Deploying to pages from @ localizethedocs/sphinx-docs-l10n@f67107de27f31e9e17bdf4cbfcd15ca46b091ed4 🚀

---
+++ ltd-config.js
@@ -0,0 +1,28 @@
+"use strict";
+
+var CONFIG_OPTIONS = {
+  CONFIG_LANGUAGES: [
+    ["en-us",   "English"],
+    ["zh-cn",   "简体中文"],
+    ["zh-tw",   "繁體中文"],
+  ],
+  CONFIG_VERSIONS: [
+    ["master",  "Development"],
+    ["9",       "Release 9"],
+    ["8",       "Release 8"],
+    ["7",       "Release 7"],
+    ["6",       "Release 6"],
+    ["5",       "Release 5"],
+    ["4",       "Release 4"],
+    ["3",       "Release 3"],
+    ["2",       "Release 2"],
+    ["1",       "Release 1"],
+  ],
+  CONFIG_PROJECTS: [
+    ["Index",   "https://projects.localizethedocs.org"],
+    ["Crowdin", "https://localizethedocs.crowdin.com/sphinx-docs-l10n"],
+    ["GitHub",  "https://github.com/localizethedocs/sphinx-docs-l10n"],
+    ["GitCode", "https://gitcode.com/localizethedocs/sphinx-docs-l10n"],
+    ["GitFlic", "https://gitflic.ru/project/localizethedocs/sphinx-docs-l10n"],
+  ]
+};
